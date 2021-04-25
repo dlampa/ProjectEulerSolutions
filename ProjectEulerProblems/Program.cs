8c5fb50bb55b5637d753c302fa1e15d7df51ab8a
Damir Lampa <lampa@altx.dev>
2021-04-24 19:12:12 -0600

    Solved Problem4;

--- ProjectEulerProblems/Program.cs
+++ ProjectEulerProblems/Program.cs
@@ -10,6 +10,7 @@ namespace ProjectEulerSolutions
             Console.WriteLine($"Problem 1: {Problem1.Solve()}");
             Console.WriteLine($"Problem 2: {Problem2.Solve()}");
             Console.WriteLine($"Problem 3: {Problem3.Solve()}");
+            Console.WriteLine($"Problem 4: {Problem4.Solve()}");
             Console.WriteLine($"Problem 8: {Problem8.Solve()}");
         }
 
---
+++ ProjectEulerProblems/Solutions/Problem4.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerSolutions.Solutions
+{
+    public static class Problem4
+    {
+        /*
+         A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+         Find the largest palindrome made from the product of two 3-digit numbers.
+        */
+        public static int Solve(bool output = false)
+        {
+            // Largest product of two 3-digit numbers
+            int result = 998001;
+            char[] revNum = new char[6];
+            bool match = false;
+
+            while ( ! match )
+            {
+                // Reverse the array of chars
+                revNum = result.ToString().ToCharArray();
+                Array.Reverse(revNum);
+
+                // Is it a palindrome?
+                if (output) Console.WriteLine($"{new string(revNum)}:{result}");
+                bool palindrome = new string (revNum) == result.ToString();
+
+                if (palindrome) {
+                    for (int i = 999; i > 99; i--)
+                    {
+                        // Is it a product of two three digit numbers?
+                        if ((result % i == 0) && ( result / i < 1000 ))
+                        {
+                            if (output) Console.WriteLine(i);
+                            match = true;
+                            break;
+                        }
+                    }
+                    if (!match) result -= 1;
+                } else result -= 1;
+                
+            }
+            
+            return result;
+        }
+
+    }
+}
