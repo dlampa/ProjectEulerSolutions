c31798f647f43f39b58ca0c95eb66df0b4d94b10
Damir Lampa <lampa@altx.dev>
2021-04-24 22:39:17 -0600

    Solved Problem5;

--- ProjectEulerProblems/Program.cs
+++ ProjectEulerProblems/Program.cs
@@ -11,6 +11,7 @@ namespace ProjectEulerSolutions
             Console.WriteLine($"Problem 2: {Problem2.Solve()}");
             Console.WriteLine($"Problem 3: {Problem3.Solve()}");
             Console.WriteLine($"Problem 4: {Problem4.Solve()}");
+            Console.WriteLine($"Problem 5: {Problem5.Solve()}");
             Console.WriteLine($"Problem 8: {Problem8.Solve()}");
         }
 
---
+++ ProjectEulerProblems/Solutions/Problem5.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerSolutions.Solutions
+{
+    public static class Problem5
+    {
+        public static int Solve(bool output = false)
+        {
+            int num = 2520;
+
+            bool[] match = new bool[11];
+                        
+            while (!match.All(x => x == true))
+            {
+                // If the number is divisible by 10 - 20 it will be divisible by 1-20
+                for (int i = 10; i <= 20; i++)
+                {
+                    // Store result of each division in a bool array
+                    match[i - 10] = (num % i == 0);
+
+                    // Stop checking num if num % i is not true
+                    if (!match[i - 10])
+                    {
+                        // The number has to be even to be divisible by 2 and 10, so the next iteration is +10
+                        num += 10;
+                        match = new bool[11];
+                        break;
+                    }
+                }
+             
+            }
+
+            return num; 
+        }
+    }
+}
