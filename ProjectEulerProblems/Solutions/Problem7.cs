3d7586c8fc067e98be33e04479af88f37842b791
Damir Lampa <lampa@altx.dev>
2021-04-24 23:16:01 -0600

    Added Problem7; Added question to Problem6;

---
+++ ProjectEulerProblems/Solutions/Problem7.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerSolutions.Solutions
+{
+    public static class Problem7
+    {
+        public static 
+    }
+}
